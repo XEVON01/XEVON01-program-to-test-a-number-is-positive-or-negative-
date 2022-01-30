14f904192b565eb523895d0e92cdab303ec50e35
XEVON <87572659+XEVON01@users.noreply.github.com>
2022-01-30 12:42:25 +0530

    Add files via upload

---
+++ program to test a number is positive or not.py
@@ -0,0 +1,12 @@
+name=("aadi")
+enter_name=input("enter your name:")
+if enter_name==name:
+    password=("4200")
+    enter_pass=input("enter your password")
+    if enter_pass!=password:
+        print("wrong password")
+    else:
+        print("code=4200")
+        
+else:
+    print("wrong username")
